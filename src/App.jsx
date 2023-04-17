5658b03dae0881688e43a0247028f03aeeb00d52
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2023-04-17 14:37:33 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import Button from "./Button";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+
+function App() {
+  const handleClick = () => {
+    console.log(1555);
+  };
+  const handleHover = () => {
+    console.log(32535435);
+  };
+
+  return (
+    <div>
+      <div>
+        <Button
+          className="mb-5"
+          onClick={handleClick}
+          onMouseOver={handleHover}
+          primary
+          rounded
+        >
+          <GoBell /> Click me!!!
+        </Button>
+      </div>
+      <div>
+        <Button success outline>
+          <GoCloudDownload />
+          Buy
+        </Button>
+      </div>
+      <div>
+        <Button warning rounded>
+          <GoDatabase />
+          Go
+        </Button>
+      </div>
+      <div>
+        <Button secondary>Search</Button>
+      </div>
+      <div>
+        <Button danger>aaaaa</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
